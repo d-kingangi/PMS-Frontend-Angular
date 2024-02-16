db27d94a3923634da6164f583d7494d3c3b05823
d-kingangi <itsronduncan@gmail.com>
2024-02-16 11:28:50 +0300

    assignment

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, NavbarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    title = 'Login'
 
+    loginForm: FormGroup
+
+    constructor(private fb:FormBuilder){ 
+      this.loginForm = this.fb.group({
+      email:['', [Validators.required, Validators.email]],
+      password:['', [ Validators.required ]]
+    })
+  }
 }
--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { users } from '../../interfaces/user.inteface';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ NavbarComponent, FormsModule, RouterLink, CommonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+    title = 'Register Here' 
 
+  
 }
